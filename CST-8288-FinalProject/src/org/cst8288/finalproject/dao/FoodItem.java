20759030443ac49156cb3a62a9696c693ba6a906
enwolf <35503014+enwolf@users.noreply.github.com>
2024-03-26 19:41:10 -0400

    Updated Java Class File: FoodItem.java
    This class extends Item.java
    
    Implemented required methods of parent class

--- CST-8288-FinalProject/src/org/cst8288/finalproject/dao/FoodItem.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/dao/FoodItem.java
@@ -1,5 +1,102 @@
 package org.cst8288.finalproject.dao;
 
-public class FoodItem {
+import java.util.Date;
 
+import org.cst8288.dto.Item;
+
+public class FoodItem extends Item
+{
+
+	 // Constructor
+    public FoodItem() 
+    {
+
+    }
+
+    @Override
+    public int getItemID() 
+    {
+        return super.itemID;
+    }
+
+    @Override
+    public String getRetailerID() 
+    {
+        return super.retailerID;
+    }
+
+    @Override
+    public String getItemName() 
+    {
+        return super.itemName;
+    }
+
+    @Override
+    public int getQuantity() 
+    {
+        return super.quantity;
+    }
+
+    @Override
+    public Date getExpirationDate() 
+    {
+        return super.expirationDate;
+    }
+
+    @Override
+    public double getPrice() 
+    {
+        return super.price;
+    }
+
+    @Override
+    public boolean isSurplus() 
+    {
+        return super.isSurplus;
+    }
+
+    @Override
+    public void setItemID(int itemID) 
+    {
+        super.itemID = itemID;
+    }
+
+    @Override
+    public void setRetailerID(String retailerID) 
+    {
+        super.retailerID = retailerID;
+    }
+
+    @Override
+    public void setItemName(String itemName) 
+    {
+        super.itemName = itemName;
+    }
+
+    @Override
+    public void setQuantity(int quantity) 
+    {
+        super.quantity = quantity;
+    }
+
+    @Override
+    public void setExpirationDate(Date expirationDate) 
+    {
+        super.expirationDate = expirationDate;
+    }
+
+    @Override
+    public void setPrice(double price) 
+    {
+        super.price = price;
+    }
+
+    @Override
+    public void setSurplus(boolean isSurplus) 
+    {
+        super.isSurplus = isSurplus;
+    }
 }
+
+
+
