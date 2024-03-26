599e826da3362480279b48657832e076897e16d0
enwolf <35503014+enwolf@users.noreply.github.com>
2024-03-26 19:46:36 -0400

    Updated Java Interface File: InventoryManagerDAOInterface.java
    Added Interface Methods:
    
    addInventoryItem(Item inventoryItem)
    updateInventoryItem(Item inventoryItem)
    removeInventoryitem(int itemID)
    getSingleInventoryItemByID(int itemID)
    getALLInventoryItems()
    getALLSurplusInventoryItems()

--- CST-8288-FinalProject/src/org/cst8288/finalproject/dao/InventoryManagerDAOInterface.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/dao/InventoryManagerDAOInterface.java
@@ -1,7 +1,17 @@
 package org.cst8288.finalproject.dao;
 
+import java.util.List;
+
+import org.cst8288.dto.Item;
+
 public interface InventoryManagerDAOInterface {
 	
 
+	void addInventoryItem(Item inventoryItem);
+	void updateInventoryItem(Item inventoryItem);
+	void removeInventoryitem(int itemID);
+	Item getSingleInventoryItemByID(int itemID);
+	List<Item> getALLInventoryItems();
+	List<Item> getALLSurplusInventoryItems();
 
 }
