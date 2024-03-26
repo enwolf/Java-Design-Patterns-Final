2899b74baf764e4ccaa17abf305af9e9a05f2017
enwolf <35503014+enwolf@users.noreply.github.com>
2024-03-26 19:51:22 -0400

    Updated java Class File: InventoryManagerDAO.java
    InventoryManagerDAO.java implements InventoryManagerDAOInterface.java
    
    Implemented required methods, without Logic

--- CST-8288-FinalProject/src/org/cst8288/finalproject/dao/InventoryManagerDAO.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/dao/InventoryManagerDAO.java
@@ -1,5 +1,47 @@
 package org.cst8288.finalproject.dao;
 
-public class InventoryManagerDAO {
+import java.util.List;
+
+import org.cst8288.dto.Item;
+
+public class InventoryManagerDAO implements InventoryManagerDAOInterface{
+
+	@Override
+	public void addInventoryItem(Item inventoryItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateInventoryItem(Item inventoryItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeInventoryitem(int itemID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Item getSingleInventoryItemByID(int itemID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> getALLInventoryItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> getALLSurplusInventoryItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
+
+
