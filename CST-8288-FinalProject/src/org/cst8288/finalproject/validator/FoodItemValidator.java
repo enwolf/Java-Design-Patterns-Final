2b24aa487b4b17ac84becdcea24e7488bd70c3c9
enwolf <35503014+enwolf@users.noreply.github.com>
2024-03-26 21:18:45 -0400

    Updated Java Class File: FoodItemValidator.java
    Add methods and there logic:
    
    validateFoodItemId(String foodItemId)
    validateItemName(String itemName)
    validateQuantity(int quantity)
    validateExperationDate(String date)
    validatePrice(double price)
    validateSurplus(boolean isSurplus)
    validateRetailer(String retailerName)
    validateFoodItem(Item foodItem)

--- CST-8288-FinalProject/src/org/cst8288/finalproject/validator/FoodItemValidator.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/validator/FoodItemValidator.java
@@ -1,5 +1,90 @@
 package org.cst8288.finalproject.validator;
 
-public class FoodItemValidator {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+import org.cst8288.dto.Item;
+
+public class FoodItemValidator 
+{
+	
+	public FoodItemValidator() 
+	{
+		
+	}
+
+	public boolean validateFoodItemId(String foodItemId) 
+	{
+	    String regex = "\\d+"; // Regex pattern to match one or more digits
+	    return foodItemId.matches(regex);
+	}
+	
+	public boolean validateItemName(String itemName) 
+	{
+	    String regex = "^[a-zA-Z0-9!&]*$"; 
+	    return itemName.matches(regex);
+	}
+	
+	public boolean validateQuantity(int quantity) 
+	{
+        return quantity > 0;
+    }
+
+    public boolean validateExperationDate(String date) 
+    {
+    	SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        dateFormat.setLenient(false); // This ensures strict adherence to the pattern.
+        try 
+        {
+            dateFormat.parse(date); 
+            return true; 
+        }
+        catch (ParseException e) 
+        {
+            return false; 
+        }
+    }
+    
+    public boolean validatePrice(double price) 
+    {
+        return price >= 0;
+    }
+    
+    public boolean validateSurplus(boolean isSurplus) 
+    {
+        return true; 
+    }
+    
+    public boolean validateRetailer(String retailerName) 
+    {
+        String regex = "^[a-zA-Z0-9!&]+$"; 
+        return retailerName.matches(regex);
+    }
+        
+    public boolean validateFoodItem(Item foodItem) 
+    {      
+    	if (foodItem == null) 
+    		return false; // Return false if the FoodItem object is null
+        
+        if (!validateItemName(foodItem.getItemName())) 
+            return false; // Invalid Item Name
+                
+        if (!validateQuantity(foodItem.getQuantity())) 
+        	return false; // Invalid Quantity
+                
+        if (!validateExperationDate(foodItem.getExpirationDate().toString())) 
+           return false; // Invalid Expiration Date
+                
+        if (!validatePrice(foodItem.getPrice())) 
+           return false; // Invalid Price
+                
+        if (!validateSurplus(foodItem.isSurplus())) 
+           return false; // Invalid Surplus
+                
+        if (!validateRetailer(foodItem.getRetailerID())) 
+           return false; // Invalid Retailer Name
+        
+        return true; // All validations passed
+    }
+	
 }
