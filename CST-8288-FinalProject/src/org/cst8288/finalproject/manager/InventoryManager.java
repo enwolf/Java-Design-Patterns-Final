db008a9df69a6699754c2c784ddf7cfa77aa497f
enwolf <35503014+enwolf@users.noreply.github.com>
2024-03-26 20:31:06 -0400

    Updated Java Class File: InventoryManager.java
    Added Data Members:
    
    InventoryManagerDAO inventoryDAO;
    FoodItemValidator foodItemValidator;
    List<Item> inventoryItems;
    
    Added Parameterized constructor.
    
    Implemented Logic for Methods:
    
    addInventoryItem(Item inventoryItem)
    removeInventoryItem(int itemID)
    getInventoryItem(int itemID)
    getAllInventoryItems()
    getAllSurplusnventoryItems()
    
    TODO
    
    Methods logic left to  implement: validateAndAddInventoryItem(Item
    inventoryItem)
    
    This will require the complete java class FoodItemValidator.java

--- CST-8288-FinalProject/src/org/cst8288/finalproject/manager/InventoryManager.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/manager/InventoryManager.java
@@ -1,5 +1,53 @@
 package org.cst8288.finalproject.manager;
 
+import java.util.List;
+
+import org.cst8288.dto.Item;
+import org.cst8288.finalproject.dao.InventoryManagerDAO;
+import org.cst8288.finalproject.validator.FoodItemValidator;
+
 public class InventoryManager {
+	
+	private InventoryManagerDAO inventoryDAO;
+	private FoodItemValidator foodItemValidator;
+	private List<Item> inventoryItems;
+		
+	
+	public InventoryManager(InventoryManagerDAO inventoryDAO, FoodItemValidator foodItemValidator ) 
+	{
+		this.inventoryDAO = inventoryDAO;
+		this.foodItemValidator = foodItemValidator;
+	}	
+	
+	public void validateAndAddInventoryItem(Item inventoryItem) 
+	{
+		//TO DO create the logic once you have finished creating the FoodItemValidator class and methods.
+	}
+	
+	public void addInventoryItem(Item inventoryItem) 
+	{
+		inventoryDAO.addInventoryItem(inventoryItem);
+	}
+	
+	public void removeInventoryItem(int itemID) 
+	{
+		inventoryDAO.removeInventoryitem(itemID);		
+	}
+	
+	public void getInventoryItem(int itemID) 
+	{
+		inventoryDAO.getSingleInventoryItemByID(itemID);
+	}
+	
+	public List<Item> getAllInventoryItems()
+	{
+		return inventoryDAO.getALLInventoryItems();		
+	}
+	
+	public List<Item> getAllSurplusnventoryItems()
+	{
+		return inventoryDAO.getALLSurplusInventoryItems();
+	}
+	
 
 }
