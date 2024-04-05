979f7d04f2c5f7b78fecdcb90db2e9d01f068682
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-05 13:15:13 -0400

    Updated Java Interface File: TransactionDAOInterface.java
    Added Abstract method:
    
    void createNewTransactionRecord(int userID, int itemID, double
    amountPaid) throws SQLException;

--- CST-8288-FinalProject/src/org/cst8288/finalproject/interfaces/TransactionDAOInterface.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/interfaces/TransactionDAOInterface.java
@@ -1,8 +1,10 @@
 package org.cst8288.finalproject.interfaces;
 
+import java.sql.SQLException;
+
 public interface TransactionDAOInterface {
 
-	
+	void createNewTransactionRecord(int userID, int itemID, double amountPaid) throws SQLException;
 	
 	
 	
