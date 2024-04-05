fecb45c6899f7ada3abda25f04dbca1555b341d5
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-04 22:18:58 -0400

    Updated Java Class File: RetailerManager.java
    Added data members:
    
    private User userObject
    private InventoryManager inventoryManagerObj
    
    Added Parameterized Constructors"
    
    RetailerManager(User user, InventoryManager inventoryManager)

--- CST-8288-FinalProject/src/org/cst8288/finalproject/manager/RetailerManager.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/manager/RetailerManager.java
@@ -1,8 +1,17 @@
 package org.cst8288.finalproject.manager;
 
+import org.cst8288.finalproject.users.User;
+
 public class RetailerManager {
 
-
+	private User userObject;
+	private InventoryManager inventoryManagerObj;
+	
+	public RetailerManager(User user, InventoryManager inventoryManager) 
+	{
+		this.userObject = user;
+		this.inventoryManagerObj = inventoryManager;	
+	}
 	
 	
 	
