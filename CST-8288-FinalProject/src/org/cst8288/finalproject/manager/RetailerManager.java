2b142498c4f1bb735602ddccc21ed66036d8c2d8
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-05 13:11:12 -0400

    Updated Java Class File: RetailerManager.java
    This class now implements:
    
    UserTypeInterface.java
    
    Added Logic for required Interface Method.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/manager/RetailerManager.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/manager/RetailerManager.java
@@ -1,8 +1,10 @@
 package org.cst8288.finalproject.manager;
 
+import org.cst8288.finalproject.enums.UserType;
+import org.cst8288.finalproject.interfaces.UserTypeInterface;
 import org.cst8288.finalproject.users.User;
 
-public class RetailerManager {
+public class RetailerManager implements UserTypeInterface{
 
 	private User userObject;
 	private InventoryManager inventoryManagerObj;
@@ -11,6 +13,15 @@ public class RetailerManager {
 	{
 		this.userObject = user;
 		this.inventoryManagerObj = inventoryManager;	
+	}
+
+	@Override
+	public boolean VerifiyUserType(User user) {
+		
+		if (user.getType() == UserType.RETAILER) 
+			return true;
+		else
+			return false;
 	}
 	
 	
