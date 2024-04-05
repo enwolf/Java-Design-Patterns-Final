ff90cd91ea54c0b24f0196dcb5083a8cd00c8f96
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-05 13:09:19 -0400

    Updated Java Class File: ConsumerManager.java
    Added new data member:
    
    TransactionDAO transcationDAO:
    
    This will be used to update the transaction table when a purchase is
    made by a consumer.
    
    Updated Constructor to account for this new data member.
    
    This class now implements:
    
    UserTypeInterface.java
    
    Added Logic for required Interface Method.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/manager/ConsumerManager.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/manager/ConsumerManager.java
@@ -1,9 +1,11 @@
 package org.cst8288.finalproject.manager;
 
 import org.cst8288.finalproject.dao.TransactionDAO;
+import org.cst8288.finalproject.enums.UserType;
+import org.cst8288.finalproject.interfaces.UserTypeInterface;
 import org.cst8288.finalproject.users.User;
 
-public class ConsumerManager {
+public class ConsumerManager implements UserTypeInterface {
 	
 	private User userObject;
 	private InventoryManager inventoryManagerObj;
@@ -16,5 +18,17 @@ public class ConsumerManager {
 		this.transcationDAO = transcationDAO;
 	
 	}
+	
+
+	@Override
+	public boolean VerifiyUserType(User user) 
+	{
+		
+		if (user.getType() == UserType.CONSUMER) 
+			return true;
+		else
+			return false;
+	}
+	
 	
 }
