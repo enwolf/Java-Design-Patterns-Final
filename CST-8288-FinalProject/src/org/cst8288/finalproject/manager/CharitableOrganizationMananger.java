7ac496ae89ffa4f74a451aac34717b94607186e0
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-04 22:18:29 -0400

    Updated Java Class File: CharitableOrganizationMananger.java
    Added data members:
    
    private User userObject
    private InventoryManager inventoryManagerObj
    
    Added Parameterized Constructors"
    
    CharitableOrganizationMananger(User user, InventoryManager
    inventoryManager)

--- CST-8288-FinalProject/src/org/cst8288/finalproject/manager/CharitableOrganizationMananger.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/manager/CharitableOrganizationMananger.java
@@ -1,7 +1,18 @@
 package org.cst8288.finalproject.manager;
 
+import org.cst8288.finalproject.users.User;
+
 public class CharitableOrganizationMananger {
 	
-
+	private User userObject;
+	private InventoryManager inventoryManagerObj;
+	
+	public CharitableOrganizationMananger(User user, InventoryManager inventoryManager)
+	{
+		this.userObject = user;
+		this.inventoryManagerObj = inventoryManager;
+	}
+	
+	
 
 }
