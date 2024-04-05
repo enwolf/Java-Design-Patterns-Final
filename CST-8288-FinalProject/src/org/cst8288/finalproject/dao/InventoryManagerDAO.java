c1fef7939773be6753916f3621aa28159a4f9d19
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-04 22:31:51 -0400

    Updated Java Class File: InventoryManagerDAO.java
    Updated All methods to account for new FWRPSchema.sql table design.
    
    Each CRUD operation now accounts for new columns Price, DiscountRate and
    DiscountAmount.
    
    As these were overlooked during the initial design.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/dao/InventoryManagerDAO.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/dao/InventoryManagerDAO.java
@@ -1,5 +1,6 @@
 package org.cst8288.finalproject.dao;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,45 +14,90 @@ import org.cst8288.finalproject.dto.Item;
 
 public class InventoryManagerDAO implements InventoryManagerDAOInterface {
 
-    @Override
-    public void addInventoryItem(Item inventoryItem) throws SQLException 
-    {
-        String insertSqlQuery = "INSERT INTO inventory (retailerID, itemName, quantity, expirationDate, price, isSurplus) "
-                                + "VALUES (?, ?, ?, ?, ?, ?)";
-        try (Connection dbConnection = DataSource.getInstance().getConnectionToDatabase();
-             PreparedStatement preparedStatement = dbConnection.prepareStatement(insertSqlQuery)) 
-        {
-            preparedStatement.setString(1, inventoryItem.getRetailerID().trim());
-            preparedStatement.setString(2, inventoryItem.getItemName().trim());
-            preparedStatement.setInt(3, inventoryItem.getQuantity());
-            preparedStatement.setDate(4, new java.sql.Date(inventoryItem.getExpirationDate().getTime()));
-            preparedStatement.setDouble(5, inventoryItem.getPrice());
-            preparedStatement.setBoolean(6, inventoryItem.isSurplus());
-            preparedStatement.executeUpdate();
-        }
-    }
+	@Override
+	public void addInventoryItem(Item inventoryItem) throws SQLException {
+	    String insertSqlQuery = "INSERT INTO inventory (retailerID, itemName, quantity, expirationDate, price, isSurplus, discountRate, discountAmount) "
+	                          + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+	    try (Connection dbConnection = DataSource.getInstance().getConnectionToDatabase();
+	         PreparedStatement preparedStatement = dbConnection.prepareStatement(insertSqlQuery)) 
+	    {
+	        preparedStatement.setString(1, inventoryItem.getRetailerID().trim());
+	        preparedStatement.setString(2, inventoryItem.getItemName().trim());
+	        preparedStatement.setInt(3, inventoryItem.getQuantity());
+	        preparedStatement.setDate(4, new java.sql.Date(inventoryItem.getExpirationDate().getTime()));
+	        preparedStatement.setDouble(5, inventoryItem.getPrice());
+	        preparedStatement.setBoolean(6, inventoryItem.isSurplus());
 
-    @Override
-    public void updateInventoryItem(Item inventoryItem) throws SQLException {
-        String updateSqlQuery = "UPDATE inventory SET retailerID = ?, itemName = ?, quantity = ?, expirationDate = ?, price = ?, isSurplus = ? WHERE itemID = ?";
-        try (Connection dbConnection = DataSource.getInstance().getConnectionToDatabase();
-             PreparedStatement preparedStatement = dbConnection.prepareStatement(updateSqlQuery))
-        {
-            preparedStatement.setString(1, inventoryItem.getRetailerID().trim());
-            preparedStatement.setString(2, inventoryItem.getItemName().trim());
-            preparedStatement.setInt(3, inventoryItem.getQuantity());
-            preparedStatement.setDate(4, new java.sql.Date(inventoryItem.getExpirationDate().getTime()));
-            preparedStatement.setDouble(5, inventoryItem.getPrice());
-            preparedStatement.setBoolean(6, inventoryItem.isSurplus());
-            preparedStatement.setInt(7, inventoryItem.getItemID());
-            preparedStatement.executeUpdate();
-        }
-    }
+	        // Handling nullable BigDecimal values
+	        BigDecimal discountRate = inventoryItem.getDiscountRate();
+	        
+	        if (discountRate != null) 
+	        {
+	            preparedStatement.setBigDecimal(7, discountRate);
+	        } 
+	        else
+	        {
+	            preparedStatement.setNull(7, java.sql.Types.DECIMAL);
+	        }
+
+	        BigDecimal discountAmount = inventoryItem.getDiscountAmount();
+	        
+	        if (discountAmount != null) 
+	        {
+	            preparedStatement.setBigDecimal(8, discountAmount);
+	        }
+	        else 
+	        {
+	            preparedStatement.setNull(8, java.sql.Types.DECIMAL);
+	        }
+
+	        preparedStatement.executeUpdate();
+	    }
+	}
+
+	@Override
+	public void updateInventoryItem(Item inventoryItem) throws SQLException {
+	    String updateSqlQuery = "UPDATE inventory SET retailerID = ?, itemName = ?, quantity = ?, expirationDate = ?, price = ?, discountRate = ?, discountAmount = ? "
+	    					  + "WHERE InventoryID = ?";
+	    try (Connection dbConnection = DataSource.getInstance().getConnectionToDatabase();
+	         PreparedStatement preparedStatement = dbConnection.prepareStatement(updateSqlQuery)) 
+	    {
+	        preparedStatement.setString(1, inventoryItem.getRetailerID().trim());
+	        preparedStatement.setString(2, inventoryItem.getItemName().trim());
+	        preparedStatement.setInt(3, inventoryItem.getQuantity());
+	        preparedStatement.setDate(4, new java.sql.Date(inventoryItem.getExpirationDate().getTime()));
+	        preparedStatement.setDouble(5, inventoryItem.getPrice());
+
+	        BigDecimal discountRate = inventoryItem.getDiscountRate();
+	        if (discountRate != null) 
+	        {
+	            preparedStatement.setBigDecimal(6, discountRate);
+	        } 
+	        else 
+	        {
+	            preparedStatement.setNull(6, java.sql.Types.DECIMAL);
+	        }
+
+	        BigDecimal discountAmount = inventoryItem.getDiscountAmount();
+	        if (discountAmount != null) 
+	        {
+	            preparedStatement.setBigDecimal(7, discountAmount);
+	        } 
+	        else 
+	        {
+	            preparedStatement.setNull(7, java.sql.Types.DECIMAL);
+	        }
+
+	        preparedStatement.setInt(8, inventoryItem.getItemID());
+	        preparedStatement.executeUpdate();
+	    }
+	}
 
     @Override
     public void removeInventoryitem(int itemID) throws SQLException 
     {
-        String deleteSqlQuery = "DELETE FROM inventory WHERE itemID = ?";
+        String deleteSqlQuery = "DELETE FROM inventory "
+        					  + "WHERE InventoryID = ?";
         try (Connection dbConnection = DataSource.getInstance().getConnectionToDatabase();
              PreparedStatement preparedStatement = dbConnection.prepareStatement(deleteSqlQuery)) 
         {
@@ -61,23 +107,29 @@ public class InventoryManagerDAO implements InventoryManagerDAOInterface {
     }
 
     @Override
-    public Item getSingleInventoryItemByID(int itemID) throws SQLException 
-    {
-        Item inventoryItem = null;
-        String selectSqlQuery = "SELECT * FROM inventory WHERE itemID = ?";
+    public Item getSingleInventoryItemByID(int itemID) throws SQLException {
+        
+    	Item inventoryItem = null;
+        String selectSqlQuery = "SELECT * FROM inventory "
+        		              + "WHERE InventoryID = ?";
+        
         try (Connection dbConnection = DataSource.getInstance().getConnectionToDatabase();
              PreparedStatement preparedStatement = dbConnection.prepareStatement(selectSqlQuery)) 
         {
             preparedStatement.setInt(1, itemID);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) 
+            {
+                if (resultSet.next()) 
+                {
                     inventoryItem = new FoodItem();
-                    inventoryItem.setItemID(resultSet.getInt("itemID"));
-                    inventoryItem.setRetailerID(resultSet.getString("retailerID"));
-                    inventoryItem.setItemName(resultSet.getString("itemName"));
-                    inventoryItem.setQuantity(resultSet.getInt("quantity"));
-                    inventoryItem.setExpirationDate(resultSet.getDate("expirationDate"));
-                    inventoryItem.setPrice(resultSet.getDouble("price"));
+                    inventoryItem.setItemID(resultSet.getInt("InventoryID"));
+                    inventoryItem.setRetailerID(resultSet.getString("RetailerID"));
+                    inventoryItem.setItemName(resultSet.getString("ItemName"));
+                    inventoryItem.setQuantity(resultSet.getInt("Quantity"));
+                    inventoryItem.setExpirationDate(resultSet.getDate("ExpirationDate"));
+                    inventoryItem.setPrice(resultSet.getDouble("Price"));
+                    inventoryItem.setDiscountRate(resultSet.getBigDecimal("DiscountRate"));
+                    inventoryItem.setDiscountAmount(resultSet.getBigDecimal("DiscountAmount"));
                     inventoryItem.setSurplus(resultSet.getBoolean("isSurplus"));
                 }
             }
@@ -86,21 +138,26 @@ public class InventoryManagerDAO implements InventoryManagerDAOInterface {
     }
 
     @Override
-    public List<Item> getALLInventoryItems() throws SQLException 
-    {
-        List<Item> inventoryItems = new ArrayList<>();
+    public List<Item> getALLInventoryItems() throws SQLException {
+        
+    	List<Item> inventoryItems = new ArrayList<>();
         String selectAllSqlQuery = "SELECT * FROM inventory";
+        
         try (Connection dbConnection = DataSource.getInstance().getConnectionToDatabase();
              PreparedStatement preparedStatement = dbConnection.prepareStatement(selectAllSqlQuery);
-             ResultSet resultSet = preparedStatement.executeQuery()) {
-            while (resultSet.next()) {
+             ResultSet resultSet = preparedStatement.executeQuery()) 
+        {
+            while (resultSet.next()) 
+            {
                 Item inventoryItem = new FoodItem();
-                inventoryItem.setItemID(resultSet.getInt("itemID"));
-                inventoryItem.setRetailerID(resultSet.getString("retailerID"));
-                inventoryItem.setItemName(resultSet.getString("itemName"));
-                inventoryItem.setQuantity(resultSet.getInt("quantity"));
-                inventoryItem.setExpirationDate(resultSet.getDate("expirationDate"));
-                inventoryItem.setPrice(resultSet.getDouble("price"));
+                inventoryItem.setItemID(resultSet.getInt("InventoryID"));
+                inventoryItem.setRetailerID(resultSet.getString("RetailerID"));
+                inventoryItem.setItemName(resultSet.getString("ItemName"));
+                inventoryItem.setQuantity(resultSet.getInt("Quantity"));
+                inventoryItem.setExpirationDate(resultSet.getDate("ExpirationDate"));
+                inventoryItem.setPrice(resultSet.getDouble("Price"));
+                inventoryItem.setDiscountRate(resultSet.getBigDecimal("DiscountRate"));
+                inventoryItem.setDiscountAmount(resultSet.getBigDecimal("DiscountAmount"));
                 inventoryItem.setSurplus(resultSet.getBoolean("isSurplus"));
                 inventoryItems.add(inventoryItem);
             }
@@ -110,23 +167,28 @@ public class InventoryManagerDAO implements InventoryManagerDAOInterface {
 
 
     @Override
-    public List<Item> getALLSurplusInventoryItems() throws SQLException 
-    {
-        List<Item> surplusInventoryItems = new ArrayList<>();
-        String selectSurplusSqlQuery = "SELECT * FROM inventory WHERE isSurplus = true";
+    public List<Item> getALLSurplusInventoryItems() throws SQLException {
+        
+    	List<Item> surplusInventoryItems = new ArrayList<>();
+        String selectSurplusSqlQuery = "SELECT * FROM inventory "
+        							 + "WHERE isSurplus = true";
+        
         try (Connection dbConnection = DataSource.getInstance().getConnectionToDatabase();
              PreparedStatement preparedStatement = dbConnection.prepareStatement(selectSurplusSqlQuery);
              ResultSet resultSet = preparedStatement.executeQuery()) 
         {
-            while (resultSet.next()) 
+            
+        	while (resultSet.next()) 
             {
                 Item inventoryItem = new FoodItem();
-                inventoryItem.setItemID(resultSet.getInt("itemID"));
-                inventoryItem.setRetailerID(resultSet.getString("retailerID"));
-                inventoryItem.setItemName(resultSet.getString("itemName"));
-                inventoryItem.setQuantity(resultSet.getInt("quantity"));
-                inventoryItem.setExpirationDate(resultSet.getDate("expirationDate"));
-                inventoryItem.setPrice(resultSet.getDouble("price"));
+                inventoryItem.setItemID(resultSet.getInt("InventoryID"));
+                inventoryItem.setRetailerID(resultSet.getString("RetailerID"));
+                inventoryItem.setItemName(resultSet.getString("ItemName"));
+                inventoryItem.setQuantity(resultSet.getInt("Quantity"));
+                inventoryItem.setExpirationDate(resultSet.getDate("ExpirationDate"));
+                inventoryItem.setPrice(resultSet.getDouble("Price"));
+                inventoryItem.setDiscountRate(resultSet.getBigDecimal("DiscountRate"));
+                inventoryItem.setDiscountAmount(resultSet.getBigDecimal("DiscountAmount"));
                 inventoryItem.setSurplus(resultSet.getBoolean("isSurplus"));
                 surplusInventoryItems.add(inventoryItem);
             }
