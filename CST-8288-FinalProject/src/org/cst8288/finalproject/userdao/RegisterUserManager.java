b404f262e795880243600358a8b2ae720cc2fbe1
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-16 23:09:06 -0400

    Updated Java Class File: RegisteredUserManager.java
    Updated method registerUser()
    
    It now takes a user object directly, logic updated to use take advantage
    of the updated validateUser(User user) found in UserValidator.java.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/userdao/RegisterUserManager.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/userdao/RegisterUserManager.java
@@ -1,30 +1,31 @@
 package org.cst8288.finalproject.userdao;
 
-import org.cst8288.finalproject.users.UserInterface;
+import org.cst8288.finalproject.users.User;
 import org.cst8288.finalproject.validator.UserValidator;
 
 public class RegisterUserManager {
-    private ManageUserDAO manageUserDAO;
+    
+	private ManageUserDAOInterface manageUserDAO;
     private UserValidator userValidator;
 
-    public RegisterUserManager() {
-        this.manageUserDAO = new ManageUserDAO();
-        this.userValidator = new UserValidator();
+    public RegisterUserManager(ManageUserDAOInterface manageUserDAO, UserValidator userValidator) {
+        
+    	this.manageUserDAO = manageUserDAO;
+        this.userValidator = userValidator;
     }
 
-    public void registerUser(int userID, String userFirstName, String userLastName, String emailAddress, UserInterface.UserType userType) {
-        if (userValidator.validateUserID(userID) &&
-            userValidator.validateUserFirstName(userFirstName) &&
-            userValidator.validateUserLastName(userLastName) &&
-            userValidator.validateUserEmailAddress(emailAddress) &&
-            userValidator.validateUserType(userType)) {
-            UserInterface user = new UserInterface(userID, userFirstName, userLastName, emailAddress, userType);
+    public void registerUser(User user) 
+    {
+
+        if (userValidator.validateUser(user)) 
+        {
+
             manageUserDAO.addUser(user);
             System.out.println("User registered successfully.");
-        } else {
+        }
+        else 
+        {
             System.out.println("User registration failed. Please check the input data.");
         }
     }
-
-    
 }
