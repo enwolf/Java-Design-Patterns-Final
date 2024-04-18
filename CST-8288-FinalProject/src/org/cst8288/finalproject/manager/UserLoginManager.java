7f79b354780b56bc242f130dbcf5af0b278e7df7
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-17 21:57:07 -0400

    Updated Java Class File: UserLoginManager.java
    Updated signature for method loginUser()
    
    Small updated in logic as well since UserPasswordManager method
    signature was updated as well

--- CST-8288-FinalProject/src/org/cst8288/finalproject/manager/UserLoginManager.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/manager/UserLoginManager.java
@@ -11,17 +11,21 @@ public class UserLoginManager {
         this.userPasswordManager = userPasswordManager;
     }
 
-    public boolean loginUser(int userID, String password) {
-        if (userPasswordManager.verifyPassword(userID, password)) {
+    public boolean loginUser(String userEmail, String password) {
+        if (userPasswordManager.verifyPassword(userEmail, password)) 
+        {
             System.out.println("User logged in successfully.");
             return true;
-        } else {
+        } 
+        else 
+        {
             System.out.println("User login failed. Invalid credentials.");
             return false;
         }
     }
 
-    public void logoutUser(int userID) {
+    public void logoutUser(int userID) 
+    {
         manageUserDAO.removeUser(userID);
         System.out.println("User logged out.");
     }
