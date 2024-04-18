7db07cda3dc80c99acbf26aed6e460432632bc63
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-17 21:55:38 -0400

    Updated Java Interface File: UserAuthenticationDAOInterface.java
    Minor code formating update.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/interfaces/UserAuthenticationDAOInterface.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/interfaces/UserAuthenticationDAOInterface.java
@@ -1,7 +1,7 @@
 package org.cst8288.finalproject.interfaces;
 
 public interface UserAuthenticationDAOInterface {
-    void setPassword(int userID, String password);
     
-    boolean verifyPassword(int UserID, String password);
+	void setPassword(int userID, String password);    
+    boolean verifyPassword(String usereEmail, String password);
 }
