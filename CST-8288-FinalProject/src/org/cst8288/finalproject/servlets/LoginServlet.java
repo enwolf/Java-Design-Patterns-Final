e37189a8f962e4ec7261098f880511f46da885d6
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-17 22:03:33 -0400

    Updated Java Servlet File: LoginServlet.java
    Overridden Methods:
    init()
    doGet()
    doPost()
    
    init() Sets up our Manager, DAO and Logger classes.
    
    doGet() handles forwarding to login page.
    
    doPost() handles verifying and extracting user data once login is
    complete, it also saves the user Object into the HTTP Session Object for
    later use, this may change later but for now is good enough.
    
    Added Log output as well.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/servlets/LoginServlet.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/servlets/LoginServlet.java
@@ -1,5 +1,73 @@
 package org.cst8288.finalproject.servlets;
 
-public class LoginServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.cst8288.finalproject.dao.ManageUserDAO;
+import org.cst8288.finalproject.dao.UserAuthenticationDAO;
+import org.cst8288.finalproject.logger.LMSLogger;
+import org.cst8288.finalproject.manager.UserPasswordManager;
+import org.cst8288.finalproject.users.AbstractUser;
+
+public class LoginServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private UserPasswordManager userPasswordManager;
+    private ManageUserDAO manageUserDAO;
+    private LMSLogger logger = LMSLogger.getInstance();
+
+    public void init() 
+    {
+        UserAuthenticationDAO userAuthDAO = new UserAuthenticationDAO();
+        this.userPasswordManager = new UserPasswordManager(userAuthDAO);
+        this.manageUserDAO = new ManageUserDAO();
+        logger.debug("LoginServlet initialized with UserPasswordManager and ManageUserDAO.");
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        logger.debug("Forwarding to login page.");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/login.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        logger.debug("Context Path: " + request.getContextPath());
+        logger.debug("Attempting login for email: " + email);
+
+        boolean isValidUser = userPasswordManager.verifyPassword(email, password);
+
+        if (isValidUser) 
+        {
+            logger.info("Login successful for email: " + email);
+        
+            AbstractUser user = manageUserDAO.returnUserByEmail(email);
+            
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            
+            logger.debug("User details added to session. Redirecting to user dashboard.");
+            response.sendRedirect(request.getContextPath() + "/jsp/userDashboard.jsp");
+        } 
+        else 
+        {
+            logger.warn("Login attempt failed for email: " + email + ". Invalid email or password.");
+            
+            request.setAttribute("errorMessage", "Invalid email or password!");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/login.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
 }
