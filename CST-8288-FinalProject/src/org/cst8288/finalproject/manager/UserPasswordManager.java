22aab016e0fd5c78d49f573f3c585d6b7db97d1b
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-17 21:57:58 -0400

    Updated Java Class File: UserPasswordManager.java
    Updated Signature of Method:
    
    verifyPassword()
    
    Minor code formating changes.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/manager/UserPasswordManager.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/manager/UserPasswordManager.java
@@ -3,22 +3,22 @@ package org.cst8288.finalproject.manager;
 import org.cst8288.finalproject.dao.UserAuthenticationDAO;
 
 public class UserPasswordManager {
-    private UserAuthenticationDAO userAuthenticationDAO;
+    
+	private UserAuthenticationDAO userAuthenticationDAO;
 
-    public UserPasswordManager(UserAuthenticationDAO userAuthenticationDAO) {
+    public UserPasswordManager(UserAuthenticationDAO userAuthenticationDAO) 
+    {
         this.userAuthenticationDAO = userAuthenticationDAO;
     }
 
-
-
-    public void setPassword(int userID, String password) {
+    public void setPassword(int userID, String password) 
+    {
         userAuthenticationDAO.setPassword(userID, password);
     }
 
-    public boolean verifyPassword(int UserID, String password) {
-        return userAuthenticationDAO.verifyPassword(UserID, password);
+    public boolean verifyPassword(String UserEmail, String password) 
+    {
+        return userAuthenticationDAO.verifyPassword(UserEmail, password);
     }
-
-    //
 
 }
