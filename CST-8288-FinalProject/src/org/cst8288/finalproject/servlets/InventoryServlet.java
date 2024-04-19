9f40340e63dd0b9bdc7b5ca8f429abf2327f4719
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-19 17:15:55 -0400

    Created New Java Servlet File: InventoryServlet.java

---
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/servlets/InventoryServlet.java
@@ -0,0 +1,10 @@
+package org.cst8288.finalproject.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class InventoryServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
