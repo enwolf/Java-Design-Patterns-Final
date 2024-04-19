cb4af15d55254aec8cb1085ac0dc9d3e83061c86
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-18 23:53:26 -0400

    Updated Java Class File: CacheFilter.java
    Extracted meta statements from JSP pages and moved them in to filter
    class.
    
    This finally resolved the issue with users being able to call a page
    that should be bound by login via the back button.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/filters/CacheFilter.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/filters/CacheFilter.java
@@ -1,5 +1,27 @@
 package org.cst8288.finalproject.filters;
 
-public class CacheFilter {
+import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class CacheFilter implements Filter {
+    
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain filter)
+            throws IOException, ServletException 
+    {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        response.setHeader("Expires", "0"); // Proxies.
+        filter.doFilter(req, res);
+    }
+
+    
 }
