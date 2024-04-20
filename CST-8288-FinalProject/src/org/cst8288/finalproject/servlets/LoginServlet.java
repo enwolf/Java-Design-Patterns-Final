b3be888cb75ccaef7d0eb53d3275aa36cfcc534d
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-20 00:16:48 -0400

    Updated Java Servlet File: LoginServlet.java
    Added Logic to handle login of different user types.

--- CST-8288-FinalProject/src/org/cst8288/finalproject/servlets/LoginServlet.java
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/servlets/LoginServlet.java
@@ -12,23 +12,23 @@ import javax.servlet.http.HttpSession;
 import org.cst8288.finalproject.dao.ManageUserDAO;
 import org.cst8288.finalproject.dao.UserAuthenticationDAO;
 import org.cst8288.finalproject.logger.LMSLogger;
-import org.cst8288.finalproject.manager.UserPasswordManager;
+import org.cst8288.finalproject.manager.UserManager;
+import org.cst8288.finalproject.service.UserDataExtractorService;
 import org.cst8288.finalproject.users.AbstractUser;
+import org.cst8288.finalproject.users.CharitableOrganization;
+import org.cst8288.finalproject.users.Consumer;
+import org.cst8288.finalproject.users.Retailer;
+import org.cst8288.finalproject.validator.UserValidator;
 
 public class LoginServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
-    private UserPasswordManager userPasswordManager;
-    private ManageUserDAO manageUserDAO;
+    private UserManager userManager;
+
     private LMSLogger logger = LMSLogger.getInstance();
 
-    public void init() 
-    {
-        UserAuthenticationDAO userAuthDAO = new UserAuthenticationDAO();
-        this.userPasswordManager = new UserPasswordManager(userAuthDAO);
-        this.manageUserDAO = new ManageUserDAO();
-        logger.debug("LoginServlet initialized with UserPasswordManager and ManageUserDAO.");
-        
+    public void init() {
+        userManager = new UserManager(new ManageUserDAO(new UserDataExtractorService()), new UserValidator(), new UserAuthenticationDAO());
     }
 
     @Override
@@ -47,17 +47,36 @@ public class LoginServlet extends HttpServlet {
         logger.debug("Context Path: " + request.getContextPath());
         logger.debug("Attempting login for email: " + email);
 
-        boolean isValidUser = userPasswordManager.verifyPassword(email, password);
+        boolean isValidUser = userManager.authenticateUserPassword(email, password);
 
         if (isValidUser) 
         {
             logger.info("Login successful for email: " + email);
         
-            AbstractUser user = manageUserDAO.returnUserByEmail(email);
+            AbstractUser user = userManager.getUserByEmail(email);
             
             HttpSession session = request.getSession();
             session.setAttribute("user", user);
-            
+
+            // Handle different user types
+            switch(user.getUserType()) 
+            {
+                case RETAILER:
+                    Retailer retailerDetails = (Retailer) userManager.populateUserDetails(user);
+                    session.setAttribute("retailerDetails", retailerDetails);
+                    break;
+                case CONSUMER:
+                    Consumer consumerDetails = (Consumer) userManager.populateUserDetails(user);
+                    session.setAttribute("consumerDetails", consumerDetails);
+                    break;
+                case CHARITABLE_ORGANIZATION:
+                    CharitableOrganization charityDetails = (CharitableOrganization) userManager.populateUserDetails(user);
+                    session.setAttribute("charityDetails", charityDetails);
+                    break;
+                default:
+                    logger.debug("Standard user details stored in session.");
+                    break;
+            }
             logger.debug("User details added to session. Redirecting to user dashboard.");
             response.sendRedirect(request.getContextPath() + "/jsp/userDashboard.jsp");
         } 
