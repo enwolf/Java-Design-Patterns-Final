1cd56d6f14b264855457bd568a86ceafe8d65ff0
enwolf <35503014+enwolf@users.noreply.github.com>
2024-04-19 23:05:50 -0400

    Created New Java Servlet Class: AddItemToInventoryServlet.java

---
+++ CST-8288-FinalProject/src/org/cst8288/finalproject/servlets/AddItemToInventoryServlet.java
@@ -0,0 +1,9 @@
+package org.cst8288.finalproject.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class AddItemToInventoryServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+}
